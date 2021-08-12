a7ad8fafff6fa9bc6885cac20ce08e9079465e8a
Anand Raj ulle <anandulle@gmail.com>
2021-08-12 16:40:14 +0530

    v-2

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 from fastai.learner import load_learner
 from fastai.vision.core import PILImage
 
-model_inf = load_learner('./model/export.pkl')
+learn_inf = load_learner('./model/export.pkl')
+learn_inf.dls.vocab
 
-img = PILImage.create('./test-data/blk-AACEIDCGKIES-TCGA-AG-3881-01Z-00-DX1.jpg')
-what, _, probs = model_inf.predict(img)
+print(f'[INFO]:First image: msi')
+img = PILImage.create('./test-data/msi.jpg')
 
-print(f"001")
+pred,pred_idx,probs = learn_inf.predict(img)
+print(f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}')
 
-print(f"What is this?: {what}.")
-print(f"Probability it's a {what}: {probs[1].item():.6f}")
+print(f'[INFO]:second image: mss')
+img = PILImage.create('./test-data/mss.jpg')
 
-print()
+pred,pred_idx,probs = learn_inf.predict(img)
+print(f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}')
 
-img = PILImage.create('./test-data/ blk-AACQCATSTDFD-TCGA-CM-4746-01Z-00-DX1.jpg')
-what, _, probs = model_inf.predict(img)
 
-print(f"002")
-print(f"What is this?: {what}.")
-print(f"Probability it's a {what}: {probs[1].item():.6f}")
+
+
