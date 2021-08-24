20cc4edad975d38fa60442fc8be9485f9de1a679
Anand Raj ulle <69010611+anand-ulle@users.noreply.github.com>
2021-08-24 11:51:52 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,110 +1,39 @@
-from tkinter import * 
-import numpy as np
+import streamlit as st
+from PIL import Image
+import numpy as np 
+from fastai.tabular.learner import load_learner
+from fastai import *
+from fastai.vision import *
+import torch
 
-    
-# loading Python Imaging Library
-from PIL import ImageTk, Image  
-# To get the dialog box to open when required 
-from tkinter import filedialog
-from fastai.learner import load_learner
-from fastai.vision.core import PILImage
+st.title("Histopathological Image Classification with PyTorch")
+st.header("MSI-MSS prediction Example")
+st.text("Upload a histopathological images for classifying as MSI or no-MSS")
 
-learn_inf = load_learner('./model/export.pkl')
-learn_inf.dls.vocab
-
-# Create a window
-root = Tk()
-frame = Frame(root)
-frame.pack()
-
-# Set Title as Image Loader
-root.title("Gatro Classifier App")
-
-# Set the resolution of window
-root.geometry('670x350')
-
-frame = LabelFrame(
-    root,
-    text='Prediction of MicroSatellite Instability from WSI',
-    bg='#f0f0f0', font='san 12 bold', foreground="blue"
-    
-)
-frame.pack(expand=True, fill=BOTH)
-#global Labele
-#Label(root, text = "The Probability: ",font='roman 12 bold',foreground="blue").grid(row=12)
-#Label(root, text = "The Predicted class: ",font='roman 12 bold',foreground="blue").grid(row=13)
-# Allow Window to be resizable
-root.resizable(width = True, height = True)
-#blank1 = Entry(root)
-#blank2 = Entry(root)
-#blank1.grid(row=12, column=1)
-#blank2.grid(row=13, column=1)
-#blank2 = Entry(root).grid(row=13, column=1)
-
-def open_img():
-    # Select the Imagename  from a folder 
-    global x 
-    x = openfilename()
-    
-    
-    # opens the image
-    img = Image.open(x)    
-        
-    # resize the image and apply a high-quality down sampling filter
-    img = img.resize((250, 250), Image.ANTIALIAS)
-    
-    # PhotoImage class is used to add image to widgets, icons etc
-    img = ImageTk.PhotoImage(img)
-    # create a label
-    panel = Label(frame, image = img)
-      
-    # set the image as img 
-    panel.image = img
-    panel.grid(row = 1,column=2)
-    
-def reset():
-    blank1.delete('0',END)
-    blank2.delete('0',END)   
-    
 def predict():
-    global blank1
-    global blank2
-    blank1 = Entry(frame)
-    blank2 = Entry(frame)
-    blank1.grid(row=2, column=2)
-    blank2.grid(row=3, column=2)
-    
-    Label(frame,text = 'Probs:', font='san 12 bold', foreground="blue").grid(row=2,column=1)
-    Label(frame,text = 'class:',font='san 12 bold', foreground="blue").grid(row=3,column=1)
-     
-    
-    im = PILImage.create(x)   
-    pred,pred_idx,probs = learn_inf.predict(im)
-    np_probs = probs.cpu().detach().numpy()
-    #class_lbl = pred.cpu().detach().numpy()    
-    probability = np_probs[pred_idx]
-    blank1.insert(0, probability)
-    blank2.insert(0, pred)
-    #Entry(root,  text = "%s" %(probability) ).grid(row=2, column=1)
-    #print(f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}')
+    # load the pre trained model
+    learn_inf = load_learner('../models/export.pkl')
+    learn_inf.dls.vocab
+    pred_class,pred_idx,probs = learn_inf.predict(img)
+    st.write('predicted Class: {} '.format(pred_class) )
+    st.write('Predicted Probability:{:.3} '.format(float(probs[pred_idx])))
+def load_image(img):
+    im = Image.open(img)
+    image = np.array(im)
+    return image
 
-   
-def openfilename():
-  
-    # open file dialog box to select image
-    # The dialogue box has a title "Open"
-    filename = filedialog.askopenfilename(title ='"pen')
-    return filename
-    
-# Create a button and place it into the window using grid layout
-imopen = Button(frame, text ='open_img', command = open_img).grid(row = 4,column = 1)
-predict = Button(frame,text = "predict_img", command = predict).grid(row =4,column = 2)
-reset = Button(frame,text = "reset_val", command = reset).grid(row =4,column = 3)
+# Uploading the File to the Page
+uploadFile = st.file_uploader(label="Upload image")
 
+# Checking the Format of the page
+if uploadFile is not None:
+    global img
+    # Perform your Manupilations (In my Case applying Filters)
+    img = load_image(uploadFile)
+    st.image(img)
+    st.write("Image Uploaded Successfully")
+else:
+    st.write("Make sure you image is in JPG/PNG Format.")
 
-root.mainloop()
-
-
-
-
-
+if st.button('predict'):
+    predict()
